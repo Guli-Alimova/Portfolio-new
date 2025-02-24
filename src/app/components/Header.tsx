7687fbf822391cc45bf9136eb3d28e5b67b3af1f
Guli-Alimova <86463317+Guli-Alimova@users.noreply.github.com>
2025-02-25 00:04:13 +0500

    style:edit style errors

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,16 +3,11 @@ import { Mood } from '../common/svg';
 
 const Header = () => {
   return (
-    <div className='bg-[#EBF2FA]'>
-      <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
-        <div className='py-4 sm:py-6 flex flex-col sm:flex-row justify-between items-center text-center'>
+    <div className='container'>
+      <div className='py-4 sm:py-6 px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row justify-between items-center text-center'>
           <h3 className='pacifico font-medium text-xl sm:text-2xl'>
             Alimova <span className='gradient-text'>Gulbahor</span>
           </h3>
-          {/* <button className='p-4 sm:p-[19px] rounded-full bg-[#EBF2FA] border drop-shadow-lg'>
-            <Mood />
-          </button> */}
-        </div>
       </div>
     </div>
   );
